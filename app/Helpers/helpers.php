443c3d32d5b4a14e88833fa6e4ca54521fadf4bc
Gustavo <guhmerces@hotmail.com>
2020-02-16 17:53:21 -0300

    adding comments to helper methods

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -3,15 +3,19 @@
 function playlistBasedOnTemp($temp) {
     switch ($temp) {
         case ($temp < 10):
+            // Classic playlist
             return "5ggSdArYBwNDU95ePtnPYG";
-            break;
+            break;            
         case ($temp >= 10 && $temp <= 14):
+            // Rock playlist
             return "0dRdi9ghHuB3HUJPJ2pZNL";
             break;
         case ($temp >= 15 && $temp <= 30):
+            // Pop playlist
             return "5sTHqyG2DAwmTCopHXHRdz";
             break;
         default:
+            // Party playlist
         return "0iXxGK2xnmNyzm8WsB1Oss";
             break;
     }
